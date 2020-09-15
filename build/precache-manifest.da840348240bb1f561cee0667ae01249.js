abb8de2cdcd4a52974cc0ffa7b1b5ca863d868d8
velomad <48247308+velomad@users.noreply.github.com>
2020-09-15 11:50:06 +0530

    test 2

---
+++ build/precache-manifest.da840348240bb1f561cee0667ae01249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e534bdd87ac74f39f379720103cb5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25981f8d742d2b6208d6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76a21bdbe94536b8bb12",
+    "url": "/static/js/2.01eca909.chunk.js"
+  },
+  {
+    "revision": "294fefc116a73bc8c0609828c7d1715e",
+    "url": "/static/js/2.01eca909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25981f8d742d2b6208d6",
+    "url": "/static/js/main.48fe823d.chunk.js"
+  },
+  {
+    "revision": "7ab06c9eb01000c11c0f",
+    "url": "/static/js/runtime-main.1be47775.js"
+  }
+]);
--- index.js
+++ index.js
@@ -15,10 +15,10 @@ app.use("/test", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "godaddy-client/build")));
+	app.use(express.static(path.join(__dirname, "build")));
 
 	app.get("/*", (req, res) => {
-		res.sendFile(path.join(__dirname, "godaddy-client/build", "index.html"));
+		res.sendFile(path.join(__dirname, "build", "index.html"));
 	});
 }
 
